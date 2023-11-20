5f3b3e2a594e34263b15bc7c7b9f0a3fa46b329b
Mateo Guzman <93417257+mateoguzman04@users.noreply.github.com>
2023-11-19 16:52:05 -0800

    Add files via upload

---
+++ bst.cpp
@@ -0,0 +1,66 @@
+#include "bst.h"
+
+TemperatureNode::TemperatureNode(const std::string& d, double maxTemp, double minTemp)
+    : date(d), maxTemperature(maxTemp), minTemperature(minTemp), left(nullptr), right(nullptr) {}
+
+TemperatureBST::TemperatureBST() : root(nullptr) {}
+
+void TemperatureBST::insert(const std::string& date, double maxTemp, double minTemp) {
+    insertNode(root, date, maxTemp, minTemp);
+}
+
+void TemperatureBST::insertNode(TemperatureNode*& node, const std::string& date, double maxTemp, double minTemp) {
+    // Implementation of insertNode function
+        if (!node) {
+            node = new TemperatureNode(date, maxTemp, minTemp);
+        } else {
+            if (maxTemp > node->maxTemperature) {
+                insertNode(node->right, date, maxTemp, minTemp);
+            } else if (maxTemp < node->maxTemperature) {
+                insertNode(node->left, date, maxTemp, minTemp);
+            } else {
+                 // If max temperatures are equal, compare based on min temperature
+                if (minTemp > node->minTemperature) {
+                    insertNode(node->right, date, maxTemp, minTemp);
+                } else {
+                    insertNode(node->left, date, maxTemp, minTemp);
+                }
+            }
+        }
+}
+
+double TemperatureBST::findGreatestTemperature() {
+    // Implementation of findGreatestTemperature function
+     TemperatureNode* maxNode = findMax(root);
+        if (maxNode) {
+            return maxNode->maxTemperature;
+        } else {
+            return -1; // Return a default value indicating no temperature found
+        }
+}
+
+double TemperatureBST::findMinimumTemperature() {
+    // Implementation of findMinimumTemperature function
+           TemperatureNode* minNode = findMin(root);
+        if(minNode) {
+            return minNode->minTemperature;
+        } else {
+            return -1; // Return a default value indicating no temperature foun
+        }
+}
+
+TemperatureNode* TemperatureBST::findMax(TemperatureNode* node) {
+    // Implementation of findMax function
+    while (node->right) {
+        node = node->right;
+    }
+    return node;
+}
+
+TemperatureNode* TemperatureBST::findMin(TemperatureNode* node) {
+    // Implementation of findMin function
+    while(node->left) {
+        node = node->left;
+        }
+    return node;
+}
---
+++ bst.h
@@ -0,0 +1,32 @@
+#ifndef BST_H
+#define BST_H
+
+#include <iostream>
+#include <string>
+
+struct TemperatureNode {
+    std::string date;
+    double maxTemperature;
+    double minTemperature;
+    TemperatureNode* left;
+    TemperatureNode* right;
+
+    TemperatureNode(const std::string& d, double maxTemp, double minTemp);
+};
+
+class TemperatureBST {
+private:
+    TemperatureNode* root;
+
+    void insertNode(TemperatureNode*& node, const std::string& date, double maxTemp, double minTemp);
+    TemperatureNode* findMax(TemperatureNode* node);
+    TemperatureNode* findMin(TemperatureNode* node);
+
+public:
+    TemperatureBST();
+    void insert(const std::string& date, double maxTemp, double minTemp);
+    double findGreatestTemperature();
+    double findMinimumTemperature();
+};
+
+#endif // TEMPERATURE_BST_H
